c3ce30795d3f00c7a5e46cab07e2881d6fb73239
Fourat ZOUARI <fourat@gmail.com>
2020-10-20 22:17:54 +0100

    Released a pypi packaged version

--- setup.py
+++ setup.py
@@ -1,31 +1,35 @@
 import os
 from setuptools import setup, find_packages
 
+
 def parse_requirements(filename):
     """load requirements from a pip requirements file"""
     lineiter = (line.strip() for line in open(filename))
     return [line for line in lineiter if line and (not line.startswith("#") and not line.startswith('-'))]
 
+
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+
 setup(
-    name = "smpp.twisted",
-    version = "0.4",
-    author = "Roger Hoover",
-    author_email = "roger.hoover@gmail.com",
-    description = "SMPP 3.4 client built on Twisted",
-    license = 'Apache License 2.0',
-    packages = find_packages(exclude=["tests"]),
+    name="smpp.twisted3",
+    version="0.4",
+    author="Roger Hoover",
+    author_email="roger.hoover@gmail.com",
+    description="SMPP 3.4 client built on Twisted / Python3",
+    license='Apache License 2.0',
+    packages=find_packages(exclude=["tests"]),
     long_description=read('README.md'),
-    keywords = "smpp twisted",
-    url = "https://github.com/mozes/smpp.twisted",
-    py_modules=["smpp.twisted"],
-    include_package_data = True,
-    package_data={'smpp.twisted': ['README.md']},
-    zip_safe = False,   
-    install_requires = parse_requirements('requirements.txt'),
-    tests_require = [
+    long_description_content_type='text/markdown',
+    keywords="smpp twisted",
+    url="https://github.com/jookies/smpp.twisted",
+    py_modules=["smpp.twisted3"],
+    include_package_data=True,
+    package_data={'smpp.twisted3': ['README.md']},
+    zip_safe=False,
+    install_requires=parse_requirements('requirements.txt'),
+    tests_require=[
         'mock',
     ],
     classifiers=[
@@ -44,4 +48,3 @@ setup(
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
 )
-
